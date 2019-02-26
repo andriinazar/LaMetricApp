8fcabc4d0bf83774e9c230fb8e2ef401954d66bf
andriinazar <nazar.andrii@gmail.com>
2019-02-27 00:33:57 +0200

    UI for login screen

--- app/src/main/java/com/example/andriinazar/lametricapptest/SignInActivity.kt
+++ app/src/main/java/com/example/andriinazar/lametricapptest/SignInActivity.kt
@@ -1,12 +1,20 @@
 package com.example.andriinazar.lametricapptest
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        b_sing_in.setOnClickListener{
+            val intent = Intent(this@SignInActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
     }
 }
